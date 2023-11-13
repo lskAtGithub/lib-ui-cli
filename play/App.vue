78241001ec40eb4d10356a9cd03bc6889cc824c7
liushengkai <949919743@qq.com>
2023-11-13 22:26:29 +0800

    test: button 调试

--- play/App.vue
+++ play/App.vue
@@ -1,9 +1,28 @@
 <template>
-  <u-demo msg="测试组件"></u-demo>
+  <div class="container">
+    <u-demo msg="测试组件"></u-demo>
+  <u-button type="" @click="handleClick">按钮</u-button>
+  <u-button type="primary" @click="handleClick">按钮</u-button>
+  <u-button type="info" @click="handleClick">按钮</u-button>
+  <u-button type="danger" @click="handleClick">按钮</u-button>
+  <u-button type="warning" @click="handleClick">按钮</u-button>
+  </div>
 </template>
 
 <script setup lang="ts">
 defineOptions({
   name: 'App',
 })
+
+const handleClick = (e: MouseEvent) => {
+  console.log(e)
+}
 </script>
+
+<style>
+.container {
+  width: 300px;
+  margin: 250px auto;
+  text-align: center;
+}
+</style>
--- play/main.ts
+++ play/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import libUi from 'lib-ui'
 
 import App from './App.vue'
+import '@lib-ui/theme-chalk/src/index.scss'
 
 const app = createApp(App)
 app.use(libUi)
