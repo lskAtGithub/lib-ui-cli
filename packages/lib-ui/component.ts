863a8f2a2d156b493255805c25ccc36050547d10
liushengkai <949919743@qq.com>
2023-11-14 18:01:18 +0800

    feat： icon 组件全局注册

--- packages/lib-ui/component.ts
+++ packages/lib-ui/component.ts
@@ -2,9 +2,11 @@ import type { Plugin } from 'vue'
 
 import { UDemo } from '@lib-ui/components'
 import { UButton } from '@lib-ui/components'
+import { UIcon } from '@lib-ui/components' 
 
 
 export default [
   UDemo,
-  UButton
+  UButton,
+  UIcon
 ] as Plugin[]
