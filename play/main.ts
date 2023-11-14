a2074249893eba5211ca04310ae45c7d0fa27b27
liushengkai <949919743@qq.com>
2023-11-14 22:30:34 +0800

    test: 测试icon组件

--- play/main.ts
+++ play/main.ts
@@ -2,7 +2,8 @@ import { createApp } from 'vue'
 import libUi from 'lib-ui'
 
 import App from './App.vue'
-import '@lib-ui/theme-chalk/src/index.scss'
+// import '@lib-ui/theme-chalk/src/index.scss'
+import '../dist/theme-chalk/css/index.css'
 
 const app = createApp(App)
 app.use(libUi)
