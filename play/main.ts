ca19b5e92acb3f396967ca70166c061b7160a67b
liushengkai <949919743@qq.com>
2023-11-15 21:37:49 +0800

    test:  调试文件打包

--- play/main.ts
+++ play/main.ts
@@ -3,7 +3,7 @@ import libUi from 'lib-ui'
 
 import App from './App.vue'
 // import '@lib-ui/theme-chalk/src/index.scss'
-import '../dist/theme-chalk/css/index.css'
+import '../dist/lib-ui/theme-chalk/index.css'
 
 const app = createApp(App)
 app.use(libUi)
